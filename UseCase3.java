5e1e9c42c765066ca9ba2196bd51a7c8770f08ce
tushargajare11 <94425851+tushargajare11@users.noreply.github.com>
2022-01-19 21:05:32 +0530

    Added file for email

---
+++ UseCase3.java
@@ -0,0 +1,19 @@
+package EmailRegistration;
+import java.util.Scanner;
+
+
+public class UseCase3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your Email: ");
+        String phone = sc.next();
+        String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+        //Matching the given phone number with regular expression
+        boolean result = phone.matches(regex);
+        if(result) {
+            System.out.println("Given email-id is valid");
+        } else {
+            System.out.println("Given email-id is not valid");
+        }
+    }
+}
